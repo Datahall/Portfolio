cac19ef421f5b108c659d470680aa14f1adece1b
Guilherme Perticarari <guilherme93@gmail.com>
2018-03-10 01:20:04 -0300

    Trying useless things

--- gsheets_connector.py
+++ gsheets_connector.py
@@ -2,7 +2,6 @@ import gspread
 import pandas as pd
 
 class GSheets:
-    
     def change_connection(self, json_file_path, name):
         self.scope = ['https://spreadsheets.google.com/feeds']
         self.creds = ServiceAccountCredentials.from_json_keyfile_name(json_file_path, scope)
